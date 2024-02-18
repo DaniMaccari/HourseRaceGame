da38c40c233d623afc48d774e196057bab56bc17
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2024-02-18 17:37:37 +0100

    lobby first steps

--- HourseRace-Client/autoload/Server.gd
+++ HourseRace-Client/autoload/Server.gd
@@ -2,7 +2,7 @@ extends Node
 
 var network = NetworkedMultiplayerENet.new()
 
-var ip_address = "127.0.0.1"
+var ip_address = "127.0.0.1" #local IP
 var port = 1909
 
 func _ready():
---
+++ HourseRace-Client/scripts/scene_handler.gd
@@ -0,0 +1,75 @@
+extends Node
+
+var loader
+var time_max = 100
+
+var current_scene
+
+func _ready():
+	current_scene = load("res://scenes/MainMenu.tscn").instance()
+	add_child(current_scene)
+	current_scene.connect("scene_change", self, "handle_scene_change")
+
+func handle_scene_change(scene_name):
+	
+	match scene_name:
+		"room_scene":
+			loader = ResourceLoader.load_interactive("res://scenes/RoomScene.tscn")
+		"lobby_scene":
+			loader = ResourceLoader.load_interactive("res://scenes/Lobby.tscn")
+		"menu_scene":
+			loader = ResourceLoader.load_interactive("res://scenes/MainMenu.tscn")
+		"game_scene":
+			loader = ResourceLoader.load_interactive("res://scenes/GameScene.tscn")
+		_:
+			loader = null
+	
+	if loader == null:
+		print("scene doesnt exist")
+		return
+	
+	$progressBar.value = 0
+	$progressBar.visible = true
+	set_process(true)
+	current_scene.queue_free()
+	
+func _process(delta):
+	if loader == null:
+		set_process(false)
+		return
+	
+	var t = OS.get_ticks_msec()
+	
+	while OS.get_ticks_msec() < t + time_max:
+		var err = loader.poll()
+		if err == ERR_FILE_EOF:
+			var next_scene = loader.get_reasource()
+			loader = null
+			set_new_scene(next_scene)
+			
+		elif err == OK:
+			update_progress()
+		
+		else:
+			print(err)
+			loader = null
+			break
+
+func set_new_scene(next_scene):
+	$progressBar.visible = false
+	current_scene = next_scene.instance()
+	current_scene.connect("scene_change", self, "handle_scene_change")
+	add_child(current_scene)
+
+func update_progress():
+	var progress = (float(loader.get_stage()) / loader.get_stage_count()) * 100
+	$progressBar.value = progress
+	
+
+
+
+
+
+
+
+
